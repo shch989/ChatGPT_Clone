0feac7a9949bac4b853e933ec138b40643146376
shch989 <tankpop11@gmail.com>
2023-04-09 17:21:48 +0900

    기본 환경 세팅

---
+++ client/README.md
@@ -0,0 +1,16 @@
+## Axios
+
+```
+$ npm install axios
+```
+
+## React-Router-Dom
+
+```
+$ npm install react-router-dom
+```
+
+## Material Ui
+```
+$ npm install @mui/material @emotion/react @emotion/styled
+```
---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const colors = require('colors')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+// dotenv
+dotenv.config()
+
+// mongo connection
+connectDB()
+
+// rest object
+const app = express()
+
+// middlewares
+app.use(cors)
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+
+const PORT = process.env.PORT
+
+// listen server
+app.listen(PORT, () => {
+  console.log(
+    `Server Running in ${process.env.DEV_MODE} mode on port on ${PORT}`.bgCyan
+      .white
+  )
+})

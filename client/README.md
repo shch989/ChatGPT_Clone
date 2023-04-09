d041c9a32cdd2ac7c76ecc725cc93ce806f410b4
shch989 <tankpop11@gmail.com>
2023-04-09 23:59:44 +0900

    Navbar Mui 적용

--- client/README.md
+++ client/README.md
@@ -14,3 +14,4 @@ $ npm install react-router-dom
 ```
 $ npm install @mui/material @emotion/react @emotion/styled
 ```
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { Fragment, useMemo } from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import { createTheme } from '@mui/material'
+import { themeSettings } from './theme'
+// components
+import Navbar from './components/Navbar'
+import Homepage from './pages/Homepage'
+import Register from './pages/Register'
+// pages
+import Login from './pages/Login'
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings(), []))
   return (
-    <div>
-      <h1>Welcome to techinfoyt</h1>
-    </div>
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ThemeProvider>
+    </Fragment>
   )
 }
 
